ee974db69e956dd0872aa7c5ff6e6907455a9d56
atla5 <aidans@bu.edu>
2019-03-19 12:43:20 -0400

    unpaywall: [bugfix] reference to nonexistent variables

--- packages/unpaywall/src/unpaywall.module.js
+++ packages/unpaywall/src/unpaywall.module.js
@@ -10,18 +10,18 @@ angular.module('bulibUnpaywall', [])
       
       // obtain custom configuration information from 'unpaywallConfig' or primo-studio constant 
       var unpaywallConfig = {};
-      if(this.logToConsole && $injector.modules){
+      if($injector.modules){
         console.log($injector.modules);
       }
       if($injector.has('unpaywallConfig')){ 
-        this.logMessageToConsole("'unpaywallConfig' found: ");
+        console.log("'unpaywallConfig' found: ");
         unpaywallConfig = $injector.get('unpaywallConfig');
       }
       if($injector.has('primoExploreUnpaywallStudioConfig')){
-        this.logMessageToConsole("'primoExploreUnpaywallStudioConfig' found: ")
+        console.log("'primoExploreUnpaywallStudioConfig' found: ")
         unpaywallConfig = $injector.get('primoExploreUnpaywallStudioConfig');
       }
-      if(this.logToConsole){ console.log(unpaywallConfig); }
+      console.log(unpaywallConfig);
 
       // provide 'unpaywall' organization with default value including some context that it's from us (for rate-limiting)
       self.email = unpaywallConfig.email || "primo-explore-unpaywall@npmjs.com";
