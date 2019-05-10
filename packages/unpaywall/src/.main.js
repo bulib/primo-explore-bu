4acdf12e4e675be601f5b11f2cae02df34bba434
atla5 <aidans@bu.edu>
2019-05-10 11:44:55 -0400

    collision: [docs] adjust example usage and package tag

--- packages/unpaywall/src/.main.js
+++ packages/unpaywall/src/.main.js
@@ -1,21 +1,24 @@
-// import npm package by name (from `node_modules/`)
+// import npm package by name (recommended)
 import 'primo-explore-unpaywall';
 
-// import copy/pasted 'unpaywall.module.js' from js/
+// import copy/pasted 'unpaywall.module.js' locally from js/
 // import './unpaywall.module';
 
 //load app 'viewCustom' as a module with [] dependencies
 var app = angular.module('viewCustom', ['angularLoad', 'bulibUnpaywall']);
 
 // - configure unpaywall - //
-app.constant('unpaywallConfig', {
-  "email":"<your_username>@<your_institution>.edu",
-  "showOnResultsPage":true,
-  "showVersionLabel":true,
-  "logToConsole":true,
-  "showDebugTable":false,
-  "publishEvents":false,
-  "logEvent":function(category, action, label){
-    window.ga('send', 'event', category, action, label);
-  }
-});
+app.component('prmSearchResultAvailabilityLineAfter', {
+    template: '<bulib-unpaywall></bulib-unpaywall>'
+  })
+  .constant('unpaywallConfig', {
+    "email":"<your_username>@<your_institution>.edu",
+    "showOnResultsPage":true,
+    "showVersionLabel":true,
+    "logToConsole":true,
+    "showDebugTable":false,
+    "publishEvents":false,
+    "logEvent":function(category, action, label){
+      window.ga('send', 'event', category, action, label);
+    }
+  });
