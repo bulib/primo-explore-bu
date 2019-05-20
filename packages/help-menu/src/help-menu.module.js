efaaef43c2041667dffcf942e49522c7c2fd3f25
atla5 <aidans@bu.edu>
2019-05-20 17:26:26 -0400

    WEB-128: [docs] improve comments for code readability

--- packages/help-menu/src/help-menu.module.js
+++ packages/help-menu/src/help-menu.module.js
@@ -4,7 +4,7 @@ import {helpMenuContentDisplayTemplate, helpMenuDialogTemplate} from './help-men
 // handle optional configuration variables
 const optionalConfigName = 'helpMenuConfig';
 const optionalStudioName = 'primoExploreHelpMenuStudioConfig';
-const logEventToGoogleAnalytics = function(category, action, label){ 
+const logEventToGoogleAnalytics = function(category, action, label){
   if(window.ga){ window.ga('send','event',category, action, label); }
 }
 
@@ -21,6 +21,7 @@ let helpMenuHelper = {
     logEventToGoogleAnalytics(category, action, label);
   },
   logHelpEvent: function(action, label=window.location.pathname){
+    // prepare (and logMessage regarding) the event sent to the `logEventToAnalytics`
     let category = "help-menu";
     this.logMessage(`logging '${category}' event with action: '${action}', label:'${label}'. [publish: ${this.publishEvents}]`);
 
@@ -29,12 +30,14 @@ let helpMenuHelper = {
     }
   },
   get_entry_by_id: function(id){
+    // return a specified entry from 'list_of_elements' for the specified id (or {})
     for(let i=0; i<this.list_of_elements.length; i++){
       if(this.list_of_elements[i].id === id){ return this.list_of_elements[i]; }
     }
     return {}
   },
   override_with_config: function(config){
+    // if a custom configuration is found, update current settings to what's specified
     if(!config || !Object.keys(config)){ return; }
     if(Object.keys(config).includes("logToConsole")){ this.logToConsole = config.logToConsole; }
     if(Object.keys(config).includes("publishEvents")){ this.publishEvents = config.publishEvents; }
@@ -44,57 +47,66 @@ let helpMenuHelper = {
   }
 };
 
-const mainHelpMenuController = function(helpMenuHelper, $injector, $scope, $timeout, $mdDialog){  
+// the shared controller containing the logic for selecting, closing, and loading the help-menu 
+const mainHelpMenuController = function(helpMenuHelper, $injector, $scope, $timeout, $mdDialog){
+
   // look for the optional 'helpMenuConfig' if present
   let config = {}
   if($injector.has(optionalConfigName)){ config = $injector.get(optionalConfigName); }
   if($injector.has(optionalStudioName)){ config = $injector.get(optionalStudioName); }
   helpMenuHelper.override_with_config(config);
-  
+
   // gather items in list from helpMenuHelper
   $scope.helpContentList = helpMenuHelper.list_of_elements;
-  
+
   // modal navigation
   $scope.hide = function() { $mdDialog.hide(); };
-  $scope.back = function() { 
-    $scope.entry = null; 
-    window.top.location.hash=""; 
+  $scope.back = function() {
+    $scope.entry = null;
+    window.top.location.hash="";
   };
   $scope.openItem = function(id, count_as_click=true){
     $scope.entry = helpMenuHelper.get_entry_by_id(id);
     if(count_as_click){ helpMenuHelper.logHelpEvent("select-item", id); }
   };
 
-  // look at the anchor tag to determine which help entry should be shown
+  // set the currently selected item from the hash in the url
   $scope.setEntryIdFromHash = function(count_as_click=true) {
     let helpOptionId = window.location.hash.substring(1);
     if(helpOptionId){ $scope.openItem(helpOptionId, count_as_click); }
   }
 
-  // trigger the help-menu to open as a popup and return focus to main page
+  // trigger the help-menu to open in a new window and return focus to main page
   $scope.openHelpInNewWindow = function(item_id=""){
     let help_event_label = window.location.pathname;
     let help_page_url = "/primo-explore/static-file/help" + window.location.search;
-    
+
     // if present, send and log the 'help-option' instead of the url
-    if(item_id){ 
-      help_page_url += "#"+item_id; 
+    if(item_id){
+      help_page_url += "#"+item_id;
       help_event_label = item_id;
     }
-    
+
+    // open the help-menu in a new-window
     let params=`width=${helpMenuHelper.helpMenuWidth},height=800,resizable=0,location=0,menubar=0,scrollbars=yes`;
     helpMenuHelper.logHelpEvent("open-window", help_event_label);
     let help_popup = open(help_page_url, 'Search Help Menu', params);
-    help_popup.addEventListener('hashchange',$scope.setEntryIdFromHash,true);
     help_popup.onload = function() { this.document.title = "Search Help Menu"; }
+
+    // prepare new window to handle 'select-option' via the '#', and close the dialog
+    help_popup.addEventListener('hashchange',$scope.setEntryIdFromHash,true);
     $scope.hide();
   }
 
-  window.addEventListener('hashchange',$scope.setEntryIdFromHash,true);
+  // after the page finishes loading, trigger a check to see which entry to open 
   $timeout($scope.setEntryIdFromHash(false), 10);
+
+  // keep listening for changes in the page's 'hash' to see which entry to show (e.g. '/path/to#hash')
+  window.addEventListener('hashchange',$scope.setEntryIdFromHash,true);
+
 }
 
-// help content by replacing everything else on the 
+// the simplest-case display for the help-menu when rendered directly into the page (`primo-explore/static-file/help`)
 angular.module('helpMenuContentDisplay', [])
   .constant('helpMenuHelper', helpMenuHelper)
   .controller('helpMenuPopupController', ['helpMenuHelper', '$injector', '$scope', '$timeout', '$mdDialog', mainHelpMenuController])
@@ -106,10 +118,16 @@ angular.module('helpMenuContentDisplay', [])
     controller: 'helpMenuPopupController'
   });
 
-// topbar button and the dialog content it triggers 
+// the topbar button and the dialog content it triggers
 angular.module('helpMenuTopbar', ['ngMaterial'])
+  
+  // add the helper used by both modules (shared)
   .constant('helpMenuHelper', helpMenuHelper)
+
+  // add the controller for the dialog once its open (shared)
   .controller('helpMenuDialogController', ['helpMenuHelper', '$injector', '$scope', '$timeout', '$mdDialog', mainHelpMenuController])
+  
+  // add a controller for the icon in the top right
   .controller('helpMenuTopbarController', ['helpMenuHelper', '$injector', '$mdDialog',
     function(helpMenuHelper, $injector, $mdDialog){
       // look for the optional 'helpMenuConfig' if present
@@ -118,8 +136,9 @@ angular.module('helpMenuTopbar', ['ngMaterial'])
       if($injector.has(optionalStudioName)){ config = $injector.get(optionalStudioName); }
       helpMenuHelper.override_with_config(config);
 
+      // open the dialog when the button in the top right is pressed
       this.openHelpMenu = function(ev){
-        helpMenuHelper.logHelpEvent( "open-dialog", window.location.pathname);
+        helpMenuHelper.logHelpEvent("open-dialog", window.location.pathname);
         $mdDialog.show({
           controller: 'helpMenuDialogController',
           template: helpMenuDialogTemplate(helpMenuHelper.helpMenuWidth),
@@ -133,12 +152,14 @@ angular.module('helpMenuTopbar', ['ngMaterial'])
       };
     }
   ])
-  .component('prmSearchBookmarkFilterAfter', {
+
+  // add the actual icon in the top right and hook the controller to it
+  .component('prmSearchBookmarkFilterAfter', {  
     template: `
       <help-menu-topbar>
         <div class="layout-align-center layout-row">
           <a class="md-icon-button button-over-dark md-button md-primoExplore-theme md-ink-ripple"
-                    aria-label="Open Search Help Menu" ng-click="$ctrl.openHelpMenu($event)" 
+                    aria-label="Open Search Help Menu" ng-click="$ctrl.openHelpMenu($event)"
                     href="#" title="open help menu">
             <prm-icon icon-type="svg" svg-icon-set="action" icon-definition="ic_help_24px"></prm-icon>
           </a>
