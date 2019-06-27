3035ddeb2ece6f0b51087a480f4850d6061157b2
atla5 <aidans@bu.edu>
2019-06-27 14:40:24 -0400

    WEB-126: ensure scripts/ return to ./
    - scripts should start and end in the same, base directory (./)
    - scripts should periodically print out the working directory in the process

--- scripts/initialize_build.sh
+++ scripts/initialize_build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-echo "installing (and moving) the latest 'ExLibrisGroup/primo-explore-devenv' into '_build/'...";
+echo "installing (and moving) the latest 'ExLibrisGroup/primo-explore-devenv' to '_build/'...";
 npm install;
 mv ./node_modules/primo-explore-devenv ./_build/;
+pwd; 
 
-echo "installing dependencies and returning...";
-cd _build;
+echo "installing _build/ dependencies and returning...";
+cd _build; pwd;
 npm install -g gulp;
 npm install;
 
 echo "updating the gulp config with our own PROXY_SERVER";
 sed -i.before_1 "s/http:\/\/il-primo17:1703/https:\/\/bu-primostage.hosted.exlibrisgroup.com:443/" ./gulp/config.js;
 sed -i.before_2 "s/http:\/\/your-server:your-port/https:\/\/bu-primostage.hosted.exlibrisgroup.com:443/g" ./gulp/config.js;
-cd ..;
+cd ..; pwd;
--- scripts/prepare_view.sh
+++ scripts/prepare_view.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-# reused variables
-view_dir="_build/primo-explore/custom/VIEW";
-pack_str="primo-explore*.tgz";
-
-echo "copying initial 'empty_view' into _build/...";
-cp -r ./empty_view/ $view_dir;
+echo "copying initial 'empty_view' into _build/..."; pwd;
+mkdir _build/primo-explore; mkdir _build/primo-explore/custom/; mkdir _build/primo-explore/custom/VIEW; #todo remove after unhidden from ExL package
+cp -r ./empty_view/* _build/primo-explore/custom/VIEW;
 
 echo "creating an 'npm pack'-ed export of each addon (./packages/) and moving it to built view";
-for d in ./packages/*/; do 
+for d in ./packages/*/; do
   cd $d; pwd;
-  npm install; 
-  npm run build || true; 
+  npm install;
+  npm run build || true;
   npm pack;
-  mv $pack_str ../../$view_dir;
-  cd ../..;
+  mv primo-explore-*.tgz ../../_build/primo-explore/custom/VIEW;
+  cd ../..; pwd;
 done;
 
 echo "'npm install'-ing each of those zips within that view"
-cd $view_dir;
+cd ./_build/primo-explore/custom/VIEW; pwd;
 npm install;  # install anything put into the empty_view package.json
-npm install $pack_str;  # install the newly packed customizations from the previous step
-rm $pack_str;
- 
+npm install primo-explore-*.tgz;  # install the newly packed customizations from the previous step
+rm primo-explore-*.tgz;
+cd ../../../..; pwd;
