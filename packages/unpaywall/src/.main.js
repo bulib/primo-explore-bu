4ce7952c2de12e30884d758a969b287575811d49
atla5 <aidans@bu.edu>
2019-10-23 13:32:27 -0400

    WEB-218: add 'imageUrl' and 'imageStyle' to unpaywallConfig

--- packages/unpaywall/src/.main.js
+++ packages/unpaywall/src/.main.js
@@ -22,5 +22,7 @@ app.component('prmSearchResultAvailabilityLineAfter', {
     "logEvent":function(category, action, label){
       window.ga('send', 'event', category, action, label);
     },
-    "labelText":"View Open Access Version"
+    "labelText":"View Open Access Version",
+    "imageUrl":"https://upload.wikimedia.org/wikipedia/commons/archive/2/25/20181007070735%21Open_Access_logo_PLoS_white.svg",
+    "imageStyle":"width: 20px; padding-right:5px; vertical-align: middle;"
   });
--- packages/unpaywall/src/unpaywall.module.js
+++ packages/unpaywall/src/unpaywall.module.js
@@ -38,6 +38,9 @@ angular.module('bulibUnpaywall', [])
 
       // customize UI/UX
       self.labelText = (Object.keys(unpaywallConfig).includes("labelText")) ? unpaywallConfig.labelText : "Open Access available via unpaywall";
+      self.imageUrl  = (Object.keys(unpaywallConfig).includes("imageUrl")) ? unpaywallConfig.imageUrl : null;
+      self.imageStyle = (Object.keys(unpaywallConfig).includes("imageStyle")) ? unpaywallConfig.imageStyle : "height: 24px; vertical-align: bottom; padding-right: 5px;";
+      
       // conditionally log to the console
       self.logMessageToConsole = function (message) {
         if (self.logToConsole) { console.log("bulib-unpaywall) " + message); }
@@ -121,7 +124,9 @@ angular.module('bulibUnpaywall', [])
         <div layout="flex" ng-if="$ctrl.best_oa_link" class="layout-row" style="margin-top: 5px;">\
           <a ng-click="$ctrl.trackLinkClick($ctrl.doi)" target="_blank" href="{{$ctrl.best_oa_link}}"\
             style="margin-left: 3px; margin-top: 3px;" rel="noreferrer">\
-            <prm-icon icon-type="svg" svg-icon-set="action" icon-definition="ic_lock_open_24px" style="color: #f68212;"></prm-icon>\
+            \
+            <img ng-if="$ctrl.imageUrl" src="{{$ctrl.imageUrl}}" alt="unpaywall logo" style="{{$ctrl.imageStyle}}">\
+            <prm-icon ng-hide="$ctrl.imageUrl" icon-type="svg" svg-icon-set="action" icon-definition="ic_lock_open_24px" style="color: #f68212;"></prm-icon>\
             \
             <span ng-if="$ctrl.labelText">{{$ctrl.labelText}}</span>\
             <span ng-hide="$ctrl.labelText"><strong>Open Access</strong> available via unpaywall</span>\
