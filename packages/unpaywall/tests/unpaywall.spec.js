b386dce0c49cff7e94849fecc946e8d56a90252b
atla5 <aidans@bu.edu>
2020-04-15 11:25:51 -0400

    WEB-339: (-) organize, add check for unpaywallController definition

--- packages/unpaywall/tests/unpaywall.spec.js
+++ packages/unpaywall/tests/unpaywall.spec.js
@@ -2,21 +2,33 @@ describe("'primo-explore-unpaywall' package smoke tests", function () {
   beforeEach(module("bulibUnpaywall"));
   beforeEach(function(){ jasmine.getJSONFixtures().fixturesPath = "base/tests/fixtures"; })
 
-  var $componentController, $rootScope, unpaywallController, prmSearchResultAvailabilityLine;
-  beforeEach(inject(function ($injector) {
-    $componentController = $injector.get("$componentController");
-    $rootScope = $injector.get("$rootScope");
+  var $componentController, $http, bindings, locals, unpaywallController, prmSearchResultAvailabilityLine;
+  beforeEach(
+    inject(function ($injector) {
+      // get/prepare helpers needed to grad/set the controller
+      $componentController = $injector.get("$componentController");
+      $http = $injector.get("$http");
+      prmSearchResultAvailabilityLine = {};
+      prmSearchResultAvailabilityLine.result = getJSONFixture("online_result.json");
 
-    // bulibUnpaywall = $injector.get("bulibUnpaywall");
-    prmSearchResultAvailabilityLine = {};
-    prmSearchResultAvailabilityLine.result = getJSONFixture("online_result.json");
-    
-    unpaywallController = $componentController('unpaywallController', null, 
-      {prmSearchResultAvailabilityLine: prmSearchResultAvailabilityLine}
-    );
-  }));
-
+      // grab the unpaywallController via [$componentController](https://docs.angularjs.org/api/ngMock/service/$componentController)
+      locals = null;
+      bindings = { 
+        $http: $http, 
+        $injector: $injector,
+        prmSearchResultAvailabilityLine: prmSearchResultAvailabilityLine 
+      };
+      unpaywallController = $componentController('unpaywallController', locals, bindings);
+    })
+  );
+  
   it("should run a sample passing test", function () {
     expect(true).toBe(true);
   });
+
+  it("should effectively load the unpaywallController", function () {
+    expect(unpaywallController).toBeDefined();
+  });
+
+
 });
