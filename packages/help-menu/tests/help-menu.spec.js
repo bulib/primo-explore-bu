c4af38ec4865653c57b8fd0680c9483f2a5ef789
atla5 <aidans@bu.edu>
2020-04-21 13:39:36 -0400

    WEB-339: (-) some progress on help-menu test module loading

--- packages/help-menu/tests/help-menu.spec.js
+++ packages/help-menu/tests/help-menu.spec.js
@@ -1,21 +1,47 @@
 describe("'primo-explore-help-menu' package smoke tests", function () {
   beforeEach(module("helpMenuTopbar"));
   beforeEach(module("helpMenuContentDisplay"));
-  
-  var $componentController, $rootScope, helpMenuContentDisplay, helpMenuTopbar, prmSearchBookmarkFilterAfter, bindings;
+
+  var $componentController, $scope, $injector, $timeout, $mdDialog, ngMaterial, helpMenuConfig, helpMenuHelper, helpMenuContentDisplay, helpMenuTopbar;
 
   beforeEach(inject(function ($injector) {
+    $injector = $injector;
     $componentController = $injector.get("$componentController");
-    $rootScope = $injector.get("$rootScope");
-    helpMenuContentDisplay = $injector.get("helpMenuContentDisplay");
-    helpMenuTopbar = $injector.get("helpMenuTopbar");
-    prmSearchBookmarkFilterAfter = {};
+    $scope = $injector.get("$rootScope");
     $timeout = $injector.get("$timeout");
+    $mdDialog = $injector.get("$mdDialog");
+    ngMaterial = {};
 
-    bindings = { };
+    helpMenuConfig = {};
+    helpMenuHelper = $injector.get("helpMenuHelper");
   }));
 
   it("should run a sample passing test", function () {
     expect(true).toBe(true);
+  });
+
+  it("should effectively load the helpMenuHelper", function () {
+    expect(helpMenuHelper).toBeDefined();
+  });
+
+  it("should effectively load the helpMenuContentDisplay", function () {
+    bindings = {
+      helpMenuHelper: helpMenuHelper,
+      $injector: $injector,
+      $scope: $scope,
+      $timeout: $timeout,
+      $mdDialog: $mdDialog
+    };
+    helpMenuContentDisplay = $componentController('helpMenuContentDisplay', null, bindings);
+    expect(helpMenuContentDisplay).toBeDefined();
+  });
+
+  it("should effectively load the helpMenuTopBar", function () {
+    bindings = { 
+      ngMaterial: ngMaterial,
+      helpMenuHelper: helpMenuHelper,
+    };
+    helpMenuTopbar = $componentController('helpMenuTopBar', null, bindings);
+    expect(helpMenuTopbar).toBeDefined();
   });
 });
